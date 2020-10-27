42658d81a1edf9b11374f9bbbebd56430755a03e
nipsalvin <68748209+nipsalvin@users.noreply.github.com>
2020-10-27 07:13:55 +0300

    Add files via upload

---
+++ hogprog.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main ()
+{
+int i = 0;
+int nmillions;
+/*do forever (for(;;))*/
+for (;;)		
+{
+	if 
+		((i%1000000) == 0){
+			nmillions = i/1000000;
+			printf("Now at %d million \n",nmillions);
+				}
+			i++;
+}			
+}
+
